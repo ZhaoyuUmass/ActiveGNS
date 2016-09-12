9e61d2c741df8524e553cf2d9544bb9f69cdf137
Zhaoyu Gao <gaozy@cs.umass.edu>
2016-09-12 11:16:01 -0400

    add a active code for computing sha256 and generating normal randoms

---
+++ scripts/activeCode/normal-random.js
@@ -0,0 +1,23 @@
+function run(value, field, querier) {
+	return value.put(field, normal_random(0,1));
+}
+
+function normal_random(mean, variance) {
+  if (mean == undefined)
+    mean = 0.0;
+  if (variance == undefined)
+    variance = 1.0;
+  var V1, V2, S;
+  do {
+    var U1 = Math.random();
+    var U2 = Math.random();
+    V1 = 2 * U1 - 1;
+    V2 = 2 * U2 - 1;
+    S = V1 * V1 + V2 * V2;
+  } while (S > 1);
+ 
+  X = Math.sqrt(-2 * Math.log(S) / S) * V1;
+  X = mean + Math.sqrt(variance) * X;
+
+  return X;
+}
