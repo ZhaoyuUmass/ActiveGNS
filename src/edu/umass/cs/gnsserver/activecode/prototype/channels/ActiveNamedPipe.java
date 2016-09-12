3c3a19f921451da59dc35d0771ce46e594c2fd6e
Zhaoyu Gao <gaozy@cs.umass.edu>
2016-09-12 17:20:16 -0400

    fix nullpointerexception

--- src/edu/umass/cs/gnsserver/activecode/prototype/channels/ActiveNamedPipe.java
+++ src/edu/umass/cs/gnsserver/activecode/prototype/channels/ActiveNamedPipe.java
@@ -73,8 +73,9 @@ public class ActiveNamedPipe implements Channel {
 	@Override
 	public Message receiveMessage() throws IOException {
 		Message am = null;
-		int len = -1;			
-		len = reader.read(readerLengthBuffer, 0, readerLengthBuffer.length);
+		int len = -1;
+		if(reader != null)
+			len = reader.read(readerLengthBuffer, 0, readerLengthBuffer.length);
 		if(len>0){
 			int length = ByteBuffer.wrap(readerLengthBuffer).getInt();
 			byte[] buffer = new byte[length];
