e91661fdaf797130cd0f8bb220ccde939aab35e6
Zhaoyu Gao <gaozy@cs.umass.edu>
2016-10-12 18:35:11 -0400

    clean up the test for 924

--- src/edu/umass/cs/gnsclient/client/testing/activecode/ActiveACLHelloWorldExample.java
+++ src/edu/umass/cs/gnsclient/client/testing/activecode/ActiveACLHelloWorldExample.java
@@ -1,5 +1,9 @@
 package edu.umass.cs.gnsclient.client.testing.activecode;
 
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,10 +16,6 @@ import org.junit.runner.notification.Failure;
 import edu.umass.cs.gnsclient.client.GNSClientCommands;
 import edu.umass.cs.gnsclient.client.util.GuidEntry;
 import edu.umass.cs.gnsclient.client.util.GuidUtils;
-import edu.umass.cs.gnscommon.AclAccessType;
-import edu.umass.cs.gnscommon.exceptions.client.ClientException;
-import edu.umass.cs.gnsserver.gnsapp.clientCommandProcessor.commandSupport.ActiveCode;
-import edu.umass.cs.gnsserver.gnsapp.clientCommandProcessor.commandSupport.MetaDataTypeName;
 
 /**
  * @author gaozy
@@ -29,11 +29,8 @@ public class ActiveACLHelloWorldExample {
 	private static GuidEntry[] entries;	
 	private static String ACCOUNT_GUID_PREFIX = "GUID";
 	private static String PASSWORD = "";
-	private static String targetGuid = "";
-	private final static String someField = "someField";
-	private final static int someValue = 1;
-	private final static String depthField = "depthField";
-	private final static String depthResult = "Depth test succeeds";
+	private final static String someField = "GUID_0_FIELD";
+	private final static String someValue = "YOU_SHOULD_NOT_SEE_THIS_FIELD";
 	
 	
 	
@@ -46,17 +43,11 @@ public class ActiveACLHelloWorldExample {
 			entries[i] = GuidUtils.lookupOrCreateAccountGuid(
 					client, ACCOUNT_GUID_PREFIX + i, PASSWORD);
 		}
-		System.out.println("Create 2 GUIDs:GUID_1 and GUID_2");
+		System.out.println("Create 2 GUIDs:GUID_0 and GUID_1");
 		
 		// initialize the fields for each guid
 		client.fieldUpdate(entries[0], someField, someValue);
-		System.out.println("Update value of field '"+someField+"' for GUID_1 to "+someValue);
-		client.fieldUpdate(entries[1], depthField, depthResult);
-		System.out.println("Update value of field '"+depthField+"' for GUID_2 to "+depthResult);
-		
-		// set the target guid to the second one and put it into the code
-		targetGuid = entries[numGuid-1].getGuid();
-		
+		System.out.println("Update value of field '"+someField+"' for GUID_0 to "+someValue);
 		
 	}
 	
@@ -75,66 +66,18 @@ public class ActiveACLHelloWorldExample {
 		}
 		
 		System.out.println(">>>>>>>>>> Testing >>>>>>>>>>");
-		String codeFile = System.getProperty("activeReadCode");
-		if(codeFile == null)
-			codeFile = "scripts/activeCode/remoteReadQuery.js";
-		
-		String code = new String(Files.readAllBytes(Paths.get(codeFile)));
-		String read_code = code.replace("//substitute this line with the targetGuid", "var targetGuid=\""+targetGuid+"\";");
-		
-		System.out.println("The code on READ action of GUID_0 is:\n"+read_code);
-		
-		
 		
 		String response = null;
-		try {
-			//client.aclAdd(AclAccessType.READ_WHITELIST, entries[0], someField, entries[0].getGuid());
-			response = client.fieldRead(entries[0].getGuid(), someField, entries[1]); //client.fieldRead(entries[0], someField);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("The response is "+response);
 		
-		//test whether remote query can bypass the ACL check	
-		/*
+		System.out.println("GUID_1 reads the field GUID_0_FIELD of GUID_0");
 		try {
-			client.activeCodeSet(entries[0].getGuid(), ActiveCode.READ_ACTION, read_code, entries[0]);			
-		} catch (ClientException e) {
-			e.printStackTrace();
-		}
-		Thread.sleep(1000);
-		
-		String response = null;
-		try {
-			response = client.fieldRead(entries[0], someField);
+			response = client.fieldRead(entries[0].getGuid(), someField, entries[1]);
+			fail("GUID_1 should not be able to access to the field GUID_0_FIELD and see the response :\""+response+"\"");
 		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		assertEquals(depthResult, response);		
-		System.out.println("Depth query test(a read followed by a read) succeeds!");
-		*/
-		
-		try {
-			cleanup();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}		
-	}
-	
-	private static void cleanup() throws Exception{
-		try {
-			client.activeCodeClear(entries[0].getGuid(), ActiveCode.READ_ACTION, entries[0]);
-			client.activeCodeClear(entries[1].getGuid(), ActiveCode.READ_ACTION, entries[1]);
-			client.activeCodeClear(entries[0].getGuid(), ActiveCode.WRITE_ACTION, entries[0]);
-			client.activeCodeClear(entries[1].getGuid(), ActiveCode.WRITE_ACTION, entries[1]);
-			client.activeCodeClear(entries[0].getGuid(), ActiveCode.ACL_ACTION, entries[0]);
-			client.activeCodeClear(entries[1].getGuid(), ActiveCode.ACL_ACTION, entries[1]);
 			
-		} catch (ClientException e) {
-			e.printStackTrace();
 		}
-				
-		client.close();
+		
+		System.out.println("Test passed!");
 	}
 	
 	/**
