354a77ce645d052a0d3b7780fdc9338008edde1d
Zhaoyu Gao <gaozy@cs.umass.edu>
2017-01-04 17:13:23 -0500

    add a donar implementation, withour fully tested

---
+++ scripts/activeCode/scheme/donar/DONAR-write.js
@@ -0,0 +1,136 @@
+/**
+ * Created by gaozy on 12/27/16.
+ */
+
+// a distance function
+function distance(lat1, lon1, lat2, lon2) {
+    if(lat1 == undefined || lat2 == undefined) return Number.MAX_VALUE;
+    var R = 6371; // Radius of the earth in km
+    var dLat = (lat2 - lat1) * Math.PI / 180;  // deg2rad below
+    var dLon = (lon2 - lon1) * Math.PI / 180;
+    var a =
+        0.5 - Math.cos(dLat)/2 +
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+        (1 - Math.cos(dLon))/2;
+
+    return R * 2 * Math.asin(Math.sqrt(a));
+}
+
+
+function sum(x, y){
+    return x+y;
+}
+
+
+function run(value, accessor, querier) {
+    if(!value.hasOwnProperty("lambda") && !value.hasOwnProperty("p")){
+        // if this is not a periodical update request for the price and redirection probability field of a DONAR mapping node
+        return value;
+    }
+
+    // constant
+    const theta = 0.01,
+        clientRegion = ["EU", "AS", "NA", "SA", "AF", "OC", "AN"];
+
+    // 1. collect the latest P_ni for all other nodes
+    // 2. collect the latest lambda_i for every replica i
+    var p = value["p"],// the traffic load contributed by all other mapping nodes, independent of this node's decision
+        // price : lambda
+        lambda = value["lambda"],
+        // weight of each replica define by user: w
+        w = querier.readGuid(null, "w")["w"],
+        // deviation for each replica: epsilon
+        epsilon = querier.readGuid(null, "epsilon")["epsilon"],
+        // # reqs from client c: can be used to calculate alpha
+        clientReqs = querier.readGuid(null, "clientReqs")["clientReqs"],
+        // the set of clients that is divided into a few groups
+        regionInfo = querier.readGuid(null, "regionInfo")["regionInfo"],
+        // the A record to fetch the information of replicas
+        replicas = querier.readGuid(null, "A")["A"]["record"],
+        dist = [],
+        alpha = [],
+        Q = [],
+        c = [],
+        A = [],
+        h = [],
+        weight = {}, // the real weight for request redirection
+        numReplicas = replicas.length,
+        numClientRegions = clientRegion.length,
+        total = numReplicas*numClientRegions,
+        s = querier.readGuid("null", "s")["s"];
+
+    // calculate alpha
+    var total = clientReqs.reduce(sum);
+    clientReqs.forEach(function(element, index){alpha[index] = element/total});
+
+    // get locations for all replicas through GeoIP
+    var coords = querier.getLocations(replicas);
+
+    // calculate the distance between client regions and replicas
+    for(i=0; i<replicas.length; i++){
+        dist.push(new Array());
+        for(var j=0; j<clientRegion.length; j++) {
+            dist[i].push(Math.round(distance(coords[replicas[i]]["latitude"], coords[replicas[i]]["longitude"],
+                regionInfo[clientRegion[j]]["latitude"], regionInfo[clientRegion[j]]["longitude"])));
+        }
+    }
+
+
+    // 3. solve optimization problem
+    var sqr = function(x) { return x*x; };
+    for(var i=0; i<total; i++){
+        Q.push([]);
+        A.push([]);
+        var xi = Math.floor(i/numReplicas),
+            xj = i%numReplicas;
+        for(var j=0; j<total; j++){
+
+            var yi = Math.floor(j/numReplicas),
+                yj = j%numReplicas;
+
+            if(xi==yi){
+                Q[i].push(lambda[xi]*sqr(s)*alpha[xj]*alpha[yj]);
+
+            }else{
+                Q[i].push(0);
+            }
+            A[i].push(0);
+        }
+        c.push(lambda[xi]*(p[xi]-w[xi])*s*alpha[xj]+s*alpha[xj]*dist[xi][xj]);
+        h.push(0);
+    }
+    // use numeric to solve the quadratic programming problem
+    var solution = numeric.solveQP(Q, c, A, h);
+    var policy = solution["unconstrained_solution"];
+    for(i=0; i<numClientRegions; i++){
+        weight[clientRegion[i]] = policy.slice(i*numReplicas, (i+1)*numReplicas);
+    }
+    value["weight"] = weight;
+    print(JSON.stringify(weight));
+
+    // 4. compute p_ni and update for this mapping node
+    var P = [];
+    for(i=0; i<numReplicas; i++){
+        var total = 0;
+        for(var j=0; j<numClientRegions; j++){
+            total += weight[clientRegion[j]][i]*alpha[j];
+        }
+        P.push(total*s);
+    }
+    value["P"] = P;
+    print(P);
+
+    // 5. Update lambda
+    for(i=0; i<numReplicas; i++){
+        var r = Math.random();
+        if(r<1/numReplicas){
+            // update
+            print("Before updating: "+i+" "+lambda);
+            lambda[i] = Math.max(0, lambda[i]+theta*(sqr(P[i]+p[i]-w[i]) - sqr(epsilon[i])));
+            print("After updating: "+i+" "+lambda);
+        }
+    }
+    // return result
+    print(JSON.stringify(value));
+    return value;
+}
---
+++ src/edu/umass/cs/gnsclient/client/testing/activecode/schemes/Donar.java
@@ -0,0 +1,190 @@
+package edu.umass.cs.gnsclient.client.testing.activecode.schemes;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import edu.umass.cs.gnsclient.client.GNSClientCommands;
+import edu.umass.cs.gnsclient.client.GNSCommand;
+import edu.umass.cs.gnsclient.client.util.GuidEntry;
+import edu.umass.cs.gnsclient.client.util.GuidUtils;
+import edu.umass.cs.gnscommon.exceptions.client.EncryptionException;
+
+/**
+ * @author gaozy
+ *
+ */
+public class Donar {
+	
+	private final static String name = "donar";
+	
+	// The continent information, get initial coordinates from Google
+	private final static String[] continents = {"EU", "AS", "NA", "SA", "AF", "OC", "AN" };
+	private final static Double[] latitudes = {54.5260, 34.0479, 54.5260, -8.7832, -8.7832, -22.7359, -82.8628 };
+	private final static Double[] longitudes = {15.2551, 100.6197, -105.2551, -55.4915, 34.5085, 140.0188, 135.0000 };
+	
+	private static JSONArray formJsonArray(List<?> l){
+		JSONArray arr = new JSONArray();
+		for(int i=0; i<l.size(); i++){
+			arr.put(l.get(i));
+		}
+		return arr;
+	}
+	
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		String readFile = "scripts/activeCode/scheme/donar/DONAR-read.js";
+		String writeFile = "scripts/activeCode/scheme/donar/DONAR-write.js";
+		String libFile = "scripts/activeCode/scheme/donar/numeric-1.2.6.js";
+		final String readCode = new String(Files.readAllBytes(Paths.get(readFile)));
+		final String writeCode = new String(Files.readAllBytes(Paths.get(writeFile)));
+		final String libCode = new String(Files.readAllBytes(Paths.get(libFile)));
+		
+		String recordFile = "conf/activeCode/donar";
+		if(System.getProperty("recordField")!=null){
+			recordFile = System.getProperty("recordFile");
+		}
+				
+		// Fields related to replica
+		List<String> records = new ArrayList<String>();
+		List<Double> w = new ArrayList<Double>();
+		List<Double> epsilon = new ArrayList<Double>();
+		List<Double> lambda = new ArrayList<Double>();
+		List<Double> p = new ArrayList<Double>();
+		List<Integer> decision = new ArrayList<Integer>();
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(recordFile)));
+		String line = reader.readLine();
+		while(line != null){
+			if(!line.startsWith("#")){
+				String[] arr = line.split(" ");
+				records.add(arr[0]);
+				w.add(Double.parseDouble(arr[1]));
+				epsilon.add(Double.parseDouble(arr[2]));
+				lambda.add(Double.parseDouble(arr[3]));
+				p.add(Double.parseDouble(arr[4]));
+				decision.add(0);
+			}				
+            line = reader.readLine();
+        } 
+		reader.close();
+		System.out.println("records:"+records+"\nweight:"+w+"\nepsilon:"+epsilon+"\nlambda:"+lambda);
+		
+		String regionFile = "conf/activeCode/donar-regions";
+		if(System.getProperty("regionField")!=null){
+			recordFile = System.getProperty("regionFile");
+		}
+		
+		// Fields related to client regions	
+		
+		reader = new BufferedReader(new InputStreamReader(new FileInputStream(regionFile)));
+		line = reader.readLine();
+		while(line != null){
+			if(!line.startsWith("#")){
+				String[] arr = line.split(" ");
+				
+			}				
+            line = reader.readLine();
+        } 
+		reader.close();
+		
+		String client_ip = args[0];
+		try {
+			InetAddress.getByName(client_ip);
+		} catch (UnknownHostException e) {
+			System.out.println("The ip address being entered is invalid, please enter an IPv4 address.");
+			System.exit(0);
+		}
+				
+		// create a client
+		GNSClientCommands client = null;
+		try {
+			client = new GNSClientCommands();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		final String ACCOUNT_GUID_SUFFIX = ".activegns.org.";
+		final String ACCOUNT_GUID = name + ACCOUNT_GUID_SUFFIX;
+		final String PASSWORD = "";
+		System.out.println("ACCOUNT_GUID:"+ACCOUNT_GUID);
+		
+		
+		// create an account
+		GuidEntry entry = null;
+		String guid_file = "guid";
+		if(System.getProperty("guid_file")!=null){
+			guid_file = System.getProperty("guid_file");
+		}
+		try {
+			ObjectInputStream input = new ObjectInputStream(new FileInputStream(new File(guid_file)));
+			entry = new GuidEntry(input);
+			input.close();
+		} catch (IOException | EncryptionException e) {
+			// the file does not exist, create a new guid
+		}
+		if(entry == null){
+			entry = GuidUtils.lookupOrCreateAccountGuid(client, ACCOUNT_GUID, PASSWORD);
+			ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(new File("guid")));
+			entry.writeObject(output);
+			output.flush();
+			output.close();
+		}
+		
+		
+		JSONObject json = new JSONObject();
+		
+		
+		// 1. create A record		
+		JSONObject recordObj = new JSONObject();
+		JSONArray arr = formJsonArray(records);
+		recordObj.put("record", arr);
+		recordObj.put("ttl", 30);		
+		json.put("A", recordObj);
+		
+		// 2. add client region information
+		JSONObject regionInfo = new JSONObject();
+		for(int i=0; i<continents.length; i++){
+			JSONObject loc = new JSONObject();
+			loc.put("latitude", latitudes[i]);
+			loc.put("longitude", longitudes[i]);
+			regionInfo.put(continents[i], loc);
+		}
+		json.put("regionInfo", regionInfo);
+		
+		/**
+		 *  3. add weight, epsilon, lambda, and p defined in conf/activeCode/donar. 
+		 *  Also update the decision field to be used by read active code.
+		 *  For the meaning of these parameters, see the details in DONAR paper
+		 */
+		
+		json.put("w", formJsonArray(w));
+		json.put("epsilon", formJsonArray(epsilon));
+		json.put("lambda", formJsonArray(lambda));
+		json.put("p", formJsonArray(p));
+		json.put("decision", decision);
+		
+		/**
+		 * 4. 
+		 */
+	}
+}
